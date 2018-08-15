cd72e4968b954ba31fde3e91535f0f816612d1cb
Anwesh43 <anweshthecool0@gmail.com>
2018-08-16 01:29:38 +0530

    adding RectRot180View in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrectrot180view/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectrot180view/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrectrot180view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rectrot180view.RectRot180View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RectRot180View.create(this)
     }
 }
