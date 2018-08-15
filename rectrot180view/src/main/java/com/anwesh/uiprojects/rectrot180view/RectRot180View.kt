0977775fad56ec3a7a8bcb93ce72eb2ab4b3e00e
Anwesh43 <anweshthecool0@gmail.com>
2018-08-16 00:45:58 +0530

    importing libraries and defining nodes

--- rectrot180view/src/main/java/com/anwesh/uiprojects/rectrot180view/RectRot180View.kt
+++ rectrot180view/src/main/java/com/anwesh/uiprojects/rectrot180view/RectRot180View.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.rectrot180view
 /**
  * Created by anweshmishra on 16/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5
