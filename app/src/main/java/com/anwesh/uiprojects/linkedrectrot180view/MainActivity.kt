3667f691f38ef0e1134186cf684494184643bc5b
Anwesh43 <anweshthecool0@gmail.com>
2018-08-16 02:59:44 +0530

    adding animationCompleteListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrectrot180view/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectrot180view/MainActivity.kt
@@ -3,14 +3,21 @@ package com.anwesh.uiprojects.linkedrectrot180view
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.rectrot180view.RectRot180View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RectRot180View.create(this)
+        val view : RectRot180View = RectRot180View.create(this)
         fullScreen()
+        view.addOnAnimationListener({createToast("animation ${it + 1} is complete")},
+                {createToast("animation ${it + 1} is reset")})
+    }
+
+    private fun createToast(msg : String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
 
