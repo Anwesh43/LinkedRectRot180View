53fc44aeb95ecd88faff24677aea3c5b0921a34e
Anwesh43 <anweshthecool0@gmail.com>
2018-08-16 01:31:34 +0530

    ading fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedrectrot180view/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrectrot180view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedrectrot180view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.rectrot180view.RectRot180View
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         RectRot180View.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
